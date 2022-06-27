377bbec3ec47a799799d2dd85569e18a7aa0f97e
xj <2635053849@qq.com>
2022-06-27 17:32:58 +0800

    third commit

--- src/main/java/xyz/xiongjun/GitTest.java
+++ src/main/java/xyz/xiongjun/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("你好2");
+        System.out.println("你好3");
     }
 }
